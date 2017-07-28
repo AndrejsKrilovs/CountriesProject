8c6bd3cc2bd9cbd41c219d9284572df7880dba58
Krilovs <aks513@de.alpha.local>
2017-07-28 14:52:31 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { ConfigurationService } from './configuration.service';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
+  providers: [ConfigurationService],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  private data;
+
+  constructor(private service: ConfigurationService) {
+    service.getData().subscribe (
+      result => this.data = result
+    );
+  }
 }
---
+++ src/app/configuration.service.ts
@@ -0,0 +1,19 @@
+import { Http, Headers, Response, Jsonp, RequestOptions } from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/catch';
+
+
+@Injectable()
+export class ConfigurationService {
+  constructor(private http: Http) {
+  }
+
+  getData = (): Observable<Response> => {
+    return this.http.get('./assets/countries_full_info.json').map(res => res.json());
+  }
+}
